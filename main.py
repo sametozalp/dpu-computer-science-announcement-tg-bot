76539c0e862e7db71d05ef7165660b4aee657176
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2024-01-12 23:57:29 +0300

    send message to tg group

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 from announcement_scraper import AnnouncementScraper
 from model.announcement import Announcement
+import telebot
 
 if __name__ == "__main__":
     url = "https://bilgisayar.dpu.edu.tr/tr/index/duyurular"
+    TOKEN = ""
+    chat_id = 0
+    
+    
     scraper = AnnouncementScraper(url)
     announcement_list = scraper.scrape_announcements()
 
-    for announcement in announcement_list:
-        print(f"Title: {announcement.title}")
-        print(f"Date: {announcement.date}")
-        print(f"Link: {announcement.link}")
-        print(f"Body: {announcement.body}")
-        print("\n")
+    bot = telebot.TeleBot(TOKEN)
+    message = announcement_list[1].title + "\n\n" + announcement_list[0].body + "\t\n\n" + announcement_list[0].date
+    bot.send_message(chat_id, message)
+
+    print("Duyuru gönderildi..")
+    #bot.polling(none_stop=True)
+
+    
+
+
+
